096a86fb8d244691b3732911b778cc5425d3e47d
Brian Winterling <bwinterling@yahoo.com>
2013-09-18 16:05:13 -0600

    Added queue print function

--- test/event_reporter_test.rb
+++ test/event_reporter_test.rb
@@ -3,25 +3,31 @@ require 'minitest/autorun'
 require './event_reporter'
 
 class EventReporterTest < MiniTest::Test
-  def test_help_command
-    reporter = EventReporter.new.process_command("help")
+
+  def test_load_csv
+    reporter = EventReporter.new
+    reporter.load_file('event_attendees.csv')
+    refute_nil reporter.file_data
+  end 
+
+  def test_initial_queue_count_equals_zero
+    reporter = EventReporter.new
+    zero = 0
+    count = reporter.queue.count
+    assert_equal count, zero
   end
 
+  test delet th
 
+  def test_help_command
+    reporter = EventReporter.new
+    help = reporter.process_command("help")   
+    refute help.nil?
+  end
 
-#load event_attendees.csv
-  # def test_load_csv
-  #   #does csv object exist within event_reporter
-  #   @loaded_csv 
-  #   assert #
-  # end 
+  def test_find_first_name_john
+  end
 
-# queue count should return 0
-  # def test_initial_queue_count_equals_zero
-
-  # end
-
-# find first_name John
 # queue count should return 63
 # queue clear
 # queue count should return 0
